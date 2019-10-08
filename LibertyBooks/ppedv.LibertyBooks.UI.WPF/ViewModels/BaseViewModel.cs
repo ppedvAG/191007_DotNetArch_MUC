1aa30254174f67ce49e0a3cd25fa608b4d1e292d
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-08 12:09:33 +0200

    Variante mit INotifyPropertyChanged

---
+++ LibertyBooks/ppedv.LibertyBooks.UI.WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.LibertyBooks.UI.WPF.ViewModels
+{
+    // Weitere Alternative:  https://github.com/Fody/PropertyChanged
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        protected virtual void SetProperty<T>(ref T field, T value, [CallerMemberName]string PropertyName = null)
+        {
+            //if (field != value)
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+            }
+        }
+
+    }
+}
--- LibertyBooks/ppedv.LibertyBooks.UI.WPF/ViewModels/MainViewModel.cs
+++ LibertyBooks/ppedv.LibertyBooks.UI.WPF/ViewModels/MainViewModel.cs
@@ -4,13 +4,15 @@ using ppedv.LibertyBooks.Logic;
 using ppedv.LibertyBooks.UI.WPF.Helpers;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ppedv.LibertyBooks.UI.WPF.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
         public MainViewModel()
         {
@@ -20,15 +22,19 @@ namespace ppedv.LibertyBooks.UI.WPF.ViewModels
 
             // Richtig: Über DependencyInjection
         }
+        private readonly Core core; // <--- Model
 
         private void GetBooks()
         {
             Books = core.GetAllBooks().ToList();
         }
 
-        private readonly Core core; // <--- Model
-
-        public List<Book> Books { get; set; }
+        private List<Book> books;
+        public List<Book> Books
+        {
+            get => books;
+            set => SetProperty(ref books, value);
+        }
         public Command GetBooksCommand { get; set; }
     }
 }
