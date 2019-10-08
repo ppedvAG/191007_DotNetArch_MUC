133990f03b613996b3570de3ce9a0a0a5d468d85
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-08 10:07:49 +0200

    Basisversion vom EFContext erstellt

--- LibertyBooks/ppedv.LibertyBooks.Data.EF/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ppedv.LibertyBooks.Data.EF
-{
-    public class Class1
-    {
-    }
-}
---
+++ LibertyBooks/ppedv.LibertyBooks.Data.EF/EFContext.cs
@@ -0,0 +1,19 @@
+﻿using ppedv.LibertyBooks.Domain;
+using System;
+using System.Data.Entity;
+
+namespace ppedv.LibertyBooks.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        // ConnectionString:
+        // TeilnehmerPC: "Server=.;Database=LibertyBooks;Trusted_Connection=true"
+        // TrainerPC: "Server=(localdb)\MSSQLLocalDB;....."
+        public EFContext() : this(@"Server=(localdb)\MSSQLLocalDB;Database=LibertyBooks;Trusted_Connection=true;AttachDbFilename=C:\temp\LibertyBooks.mdf"){}
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+        public DbSet<Store> Store { get; set; }
+    }
+}
