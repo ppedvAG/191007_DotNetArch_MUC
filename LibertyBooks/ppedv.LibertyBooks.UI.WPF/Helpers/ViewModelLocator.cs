071aada68750c22cfee85995448ce50f874f20a2
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-08 14:09:11 +0200

    ViewModelLocator: Marke Eigenbau

---
+++ LibertyBooks/ppedv.LibertyBooks.UI.WPF/Helpers/ViewModelLocator.cs
@@ -0,0 +1,50 @@
+﻿using ppedv.LibertyBooks.Data.EF;
+using ppedv.LibertyBooks.Domain.Interfaces;
+using ppedv.LibertyBooks.Logic;
+using ppedv.LibertyBooks.UI.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.LibertyBooks.UI.WPF.Helpers
+{
+    // Container
+    public class ViewModelLocator
+    {
+        // Ort für alle ViewModels und deren abhängigkeiten
+        // Bonuspunkte: IoC - Container
+        // --- Beispiel: Autofac, Unity, SimpleIoC ...
+        private Core core;
+        public Core Core
+        {
+            get
+            {
+                core = core ?? new Core(Repository);
+                return core;
+            }
+        }
+
+        private IRepository repository;
+        public IRepository Repository
+        {
+            get
+            {
+                repository = repository ?? new EFRepository(new EFContext());
+                return repository;
+            }
+        }
+
+        private MainViewModel main;
+        public MainViewModel Main
+        {
+            get
+            {
+                main = main ?? new MainViewModel(Core);
+                return main;
+            }
+        }
+
+    }
+}
