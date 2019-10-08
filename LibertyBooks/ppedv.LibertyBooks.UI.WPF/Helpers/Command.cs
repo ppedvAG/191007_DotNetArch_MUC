b38cb99b6464246e9e83e6ea0abab65888bf0777
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-08 11:53:59 +0200

    Variante ohne INotifyPropertyChanged

---
+++ LibertyBooks/ppedv.LibertyBooks.UI.WPF/Helpers/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ppedv.LibertyBooks.UI.WPF.Helpers
+{
+    public class Command : ICommand
+    {
+        public Command(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute ?? new Func<bool>(() => true);
+        }
+        private Action execute;
+        private Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
---
+++ LibertyBooks/ppedv.LibertyBooks.UI.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ppedv.LibertyBooks.Data.EF;
+using ppedv.LibertyBooks.Domain;
+using ppedv.LibertyBooks.Logic;
+using ppedv.LibertyBooks.UI.WPF.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.LibertyBooks.UI.WPF.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            // Kontrollfreak-Antipattern
+            core = new Core(new EFRepository(new EFContext()));
+            GetBooksCommand = new Command(GetBooks);
+
+            // Richtig: Über DependencyInjection
+        }
+
+        private void GetBooks()
+        {
+            Books = core.GetAllBooks().ToList();
+        }
+
+        private readonly Core core; // <--- Model
+
+        public List<Book> Books { get; set; }
+        public Command GetBooksCommand { get; set; }
+    }
+}
