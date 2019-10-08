0f0968ee9b9d9acd66511aa0b7ceed500e2ff0c7
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-08 10:17:33 +0200

    Minimaler Test für die Funktionalität vom EF erstellt

---
+++ LibertyBooks/ppedv.LibertyBooks.Data.EF.Tests/EFContextTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ppedv.LibertyBooks.Data.EF.Tests
+{
+    [TestClass]
+    public class EFContextTests
+    {
+        [TestMethod]
+        public void Can_Create_EFContext()
+        {
+            EFContext context = new EFContext();
+
+            Assert.IsNotNull(context); // Wenn sie Null ist -> Fehler
+        }
+
+        [TestMethod]
+        public void Can_Create_Database()
+        {
+            EFContext context = new EFContext();
+            if (context.Database.Exists())
+                context.Database.Delete();
+
+            Assert.IsFalse(context.Database.Exists());
+
+            context.Database.Create();
+
+            Assert.IsTrue(context.Database.Exists()); // Wenn sie Null ist -> Fehler
+        }
+    }
+}
---
+++ LibertyBooks/ppedv.LibertyBooks.Data.EF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.LibertyBooks.Data.EF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.LibertyBooks.Data.EF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e209e1e7-81d3-46f7-a5bd-fe7a01d5cfbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
